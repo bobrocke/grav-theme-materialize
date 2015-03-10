3d986d32231bb1fc1ad280a31af862bf637a2aac
Bob Rockefeller <bob@bobrockefeller.com>
2015-03-10 18:53:54 -0400

    Initial Commit

---
+++ materialize.php
@@ -0,0 +1,36 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Materialize extends Theme
+{
+	// Materializer plugin will look for this class var to know it should load
+	public $load_materializer_plugin = true;
+
+	public static function getSubscribedEvents()
+	{
+		return [
+			'onThemeInitialized' => ['onThemeInitialized', 0]
+		];
+	}
+
+	public function onThemeInitialized()
+	{
+		if ($this->isAdmin()) {
+			$this->active = false;
+			return;
+		}
+
+		$this->enable([
+			'onPageInitialized' => ['onPageInitialized', 0]
+		]);
+	}
+
+	public function onPageInitialized()
+	{
+		$this->config->set('plugins.pagination.built_in_css', false);
+		$this->config->set('plugins.relatedpages.show_score', false);
+   }
+
+}
